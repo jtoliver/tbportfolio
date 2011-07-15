56dda533be0c3fed4cab8dee1982a888dd4b0539
Joshua Toliver <josh.me.21@gmail.com>
2011-07-15 11:51:38 -0400

    changed to s3

--- app/models/home.rb
+++ app/models/home.rb
@@ -4,5 +4,8 @@ class Home < ActiveRecord::Base
 		:styles => { :medium => "300x200#",
 		:maxorg => "600x600",
 		:thumb => "220x126#",
-		:mobile => "80x80#"	}
+		:mobile => "80x80#"	},
+		:storage => :s3,
+    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+    :path => "/:style/:id/:filename"
 end
